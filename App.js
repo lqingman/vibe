a01c037ed9fdf11577000c1e4918ac1e2826e720
joy <joyjoyeux1@gmail.com>
2024-11-08 13:28:20 -0800

    created a top bar navigator in home screen

--- App.js
+++ App.js
@@ -1,23 +1,81 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Home from './Screens/Home';
 import CreatePost from './Screens/CreatePost';
 import Profile from './Screens/Profile';
 import ChangeLocation from './Screens/ChangeLocation';
+import Explore from './Screens/Explore';
+import Joined from './Screens/Joined';
 import Color from './Styles/Color';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { SafeAreaView, View } from 'react-native';
+import Style from './Styles/Style';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const TopTab = createMaterialTopTabNavigator();
 
 export default function App() {
+  // Create the Material Top Tab Navigator for the Home Screen
+  function HomeTopTabs() {
+    const insets = useSafeAreaInsets();
+    return (
+      <View
+      style={{
+        flex: 1,
+        paddingTop: 0,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+      }}
+    >
+      <TopTab.Navigator
+      initialRouteName="Explore"
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: Color.white,
+          height: 50,
+          paddingTop: 0,
+          paddingBottom: 0,
+        },
+        tabBarLabelStyle: {
+          textTransform: "capitalize",
+          fontWeight: "bold",
+          color: Color.black,
+          fontSize: 16,
+          width: "100%",
+        },
+        tabBarIndicatorStyle: {
+          height: 5,
+          borderRadius: 5,
+          backgroundColor: "#1DA1F2",
+        },
+        tabBarShowLabel: true,
+      }}>
+        <TopTab.Screen name="Explore" component={Explore} options={{
+          tabBarLabel: "Explore",
+        }}/>
+        <TopTab.Screen name="Joined" component={Joined} options={{
+            tabBarLabel: "Joined",
+          }}/>
+      </TopTab.Navigator>
+      </View>
+    );
+  }
+
   function tabNavigator() {
     return (
       // Create the bottom tab navigator 
       <Tab.Navigator
       initialRouteName="Home"
+      safeAreaInsets={{bottom: 0}}
       screenOptions={{
         tabBarActiveTintColor: Color.gold,
         tabBarActiveBackgroundColor: Color.navigatorBg,
@@ -25,9 +83,11 @@ export default function App() {
         tabBarStyle: {
           height: '8%',
           paddingBottom: 0,
+          borderTopWidth: 0,
+          elevation: 0,
         },
         tabBarItemStyle: {
-          paddingBottom: 12,
+          paddingBottom: 0,
         },
         headerStyle: {
           backgroundColor: Color.navigatorBg,
@@ -38,9 +98,10 @@ export default function App() {
       {/* Create the Home screen */}
       <Tab.Screen 
         name="Home" 
-        component={Home} 
+        component={HomeTopTabs} 
         options = {{
           title: 'Home',
+          headerShown: true,
           tabBarIcon: ({color}) => <FontAwesome5 name="home" size={24} color={color} />,
         }}
       />
@@ -65,22 +126,32 @@ export default function App() {
     )
   }
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Tab'>
-        {/* Create the tab navigator */}
-        <Stack.Screen name="Tab" children={tabNavigator} options={{headerShown: false}}/>
-        {/* Create the Home screen */}
-        <Stack.Screen name="ChangeLocation" 
-        component={ChangeLocation} 
-        options={{
-          title:"Change Your Location", 
-          headerStyle:{backgroundColor:Color.navigatorBg}, 
-          headerTintColor: Color.white,
-          headerBackTitleVisible: false,
-        }}/>
+  function StackNavigator() {
+    return(
+        <Stack.Navigator initialRouteName='Tab'>
+          {/* Create the tab navigator */}
+          <Stack.Screen name="Tab" children={tabNavigator} options={{headerShown: false}}/>
+          {/* Create the Home screen */}
+          <Stack.Screen name="ChangeLocation" 
+          component={ChangeLocation} 
+          options={{
+            title:"Change Your Location", 
+            headerStyle:{backgroundColor:Color.navigatorBg}, 
+            headerTintColor: Color.white,
+            headerBackTitleVisible: false,
+          }}/>
+        </Stack.Navigator>
+    )
+  }
 
-      </Stack.Navigator>
-    </NavigationContainer>
+  return (
+    // <SafeAreaView style={Style.appContainer} 
+    // forceInset={{ top: "always", bottom: "never" }}
+    // > 
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
--- Screens/CreatePost.js
+++ Screens/CreatePost.js
@@ -1,9 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 export default function CreatePost() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+      }}
+    >
       <Text>CreatePost</Text>
     </View>
   )
